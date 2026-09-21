7956330ad4a60af2c57abbc6b627e0474929572f
isaiah.exe <isaiah.blanc0220@gmail.com>
2026-09-21 12:10:57 -0400

    ✨ Updated tag color contrast + added color theme transition

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+/* THEME */
+
+// Switches between dark mode and light mode
 function switchTheme() {
-	document.body.classList.toggle("dark-mode")
+	// Change theme
+	document.body.classList.toggle("dark-mode");
+	document.body.classList.toggle("light-mode");
+	// Change theme button icon
+	var icon = document.getElementById("theme-icon");
+	if (document.body.classList.contains("dark-mode")) {
+		icon.classList.remove("fa-solid");
+		icon.classList.add("fa-regular");
+	} else {
+		icon.classList.remove("fa-regular");
+		icon.classList.add("fa-solid");
+	}
+	switchTagText();
 }
+
+// Triggers upon pressing the SPACE key
+window.addEventListener("keydown", (event) => {
+  if (event.code == "Space" && !event.repeat) {
+	switchTheme();
+  }
+});
